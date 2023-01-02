8f94a1c478d6decf4e7d1d1236a5a4c5a5a42f2c
Anita Spieker - Vortigo <anita.spieker.vortigo@superdigital.com.br>
2023-01-02 20:41:36 -0300

    fixed android run

--- android/app/src/main/kotlin/com/example/hymns/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/hymns/MainActivity.kt
@@ -1,13 +1,6 @@
 package com.example.hymns
 
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
-}
-
-override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-   super.configureFlutterEngine(flutterEngine)
-   GeneratedPluginRegistrant.registerWith(flutterEngine);
-   configureChannels(flutterEngine)
 }
